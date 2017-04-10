2ffc7e019acb11e5c22715ec20731dd7745f07b0
Amanda Andersson <amanda@balaz.se>
2017-04-10 15:15:42 +0200

    lagt till grafik för frågor

--- TheGreatTest/MainActivity.cs
+++ TheGreatTest/MainActivity.cs
@@ -10,9 +10,7 @@ namespace TheGreatTest
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            
              
         }
     }
